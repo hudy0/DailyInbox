296620bb34d958cc4c94d26fb19775c585576655
hudaifasaleh <hodaeyfha@gmail.com>
2023-06-20 12:48:08 +0300

    auth login setup

--- project/settings.py
+++ project/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
     DEBUG=(bool, False),
     ALLOWED_HOSTS=(list, []),
+    ACCOUNT_DEFAULT_HTTP_PROTOCOL=(str, 'https'),
 )
 environ.Env.read_env(os.path.join(BASE_DIR / 'project/.env.example'))
 
@@ -22,9 +23,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+    # THIRD_PARTY_APPS
+    "allauth",
+    "allauth.account",
     # LOCAL_APPS
     "email_service.accounts",
-    # THIRD_PARTY_APPS
 ]
 
 MIDDLEWARE = [
@@ -85,6 +89,56 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+SITE_ID = 1
+# ACCOUNT_ADAPTER = default
+# ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = default
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+# TODO: set login url
+# ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = default
+# TODO: set login redirect url
+# ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = default
+# ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = default
+# ACCOUNT_EMAIL_CONFIRMATION_HMAC = default
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_SUBJECT_PREFIX = 'EmailService'
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = env('ACCOUNT_DEFAULT_HTTP_PROTOCOL')
+# ACCOUNT_EMAIL_CONFIRMATION_COOLDOWN = default
+# ACCOUNT_EMAIL_MAX_LENGTH = default
+# ACCOUNT_MAX_EMAIL_ADDRESSES = default
+# ACCOUNT_FORMS = default
+# ACCOUNT_LOGIN_ATTEMPTS_LIMIT = default
+# ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = default
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
+# ACCOUNT_LOGOUT_ON_GET = default
+# ACCOUNT_LOGOUT_ON_PASSWORD_CHANGE = default
+# ACCOUNT_LOGIN_ON_PASSWORD_RESET = default
+# ACCOUNT_LOGOUT_REDIRECT_URL = default
+# ACCOUNT_PASSWORD_INPUT_RENDER_VALUE = default
+ACCOUNT_PRESERVE_USERNAME_CASING = False
+# ACCOUNT_PREVENT_ENUMERATION = default
+# ACCOUNT_RATE_LIMITS = default
+ACCOUNT_SESSION_REMEMBER = True
+# ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = default
+# ACCOUNT_SIGNUP_FORM_CLASS = default
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+# ACCOUNT_SIGNUP_REDIRECT_URL = default
+# ACCOUNT_TEMPLATE_EXTENSION = default
+# ACCOUNT_USERNAME_BLACKLIST = default
+# ACCOUNT_UNIQUE_EMAIL = default
+ACCOUNT_USER_DISPLAY = lambda user: user.email
+# ACCOUNT_USER_MODEL_EMAIL_FIELD = default
+# ACCOUNT_USER_MODEL_USERNAME_FIELD = default
+# ACCOUNT_USERNAME_MIN_LENGTH = default
+ACCOUNT_USERNAME_REQUIRED = False
+# ACCOUNT_USERNAME_VALIDATORS = default
+# SOCIAL_ACCOUNT_* = default
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
