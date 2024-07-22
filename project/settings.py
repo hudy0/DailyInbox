c65363caf5ab3ddfa4b41c0856d69ed7c0b562bd
hudy0 <hudaifa15@outlook.com>
2024-07-23 01:05:25 +0300

    add simple-history to track history

--- project/settings.py
+++ project/settings.py
@@ -57,7 +57,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
+    'simple_history.middleware.HistoryRequestMiddleware',
+    # "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "project.urls"
